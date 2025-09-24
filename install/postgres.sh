830bc685f6201be8a28e968e479519b934d69103
Leeroy Brun <leeroy.brun@gmail.com>
2025-09-24 15:05:48 +0200

    feat(postgres install): remove forced arch to allow arm64 installs

--- install/postgres.sh
+++ install/postgres.sh
@@ -6,7 +6,7 @@ OS_CODENAME=$(awk -F= '$1=="VERSION_CODENAME" { print $2 ;}' /etc/os-release)
 
 APT_REPO="apt.postgresql.org"
 
-echo "deb  [arch=amd64 signed-by=/etc/apt/keyrings/postgresql.asc] http://${APT_REPO}/pub/repos/apt/ ${OS_CODENAME}-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+echo "deb  [signed-by=/etc/apt/keyrings/postgresql.asc] http://${APT_REPO}/pub/repos/apt/ ${OS_CODENAME}-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc -o /etc/apt/keyrings/postgresql.asc
 
 apt-get update

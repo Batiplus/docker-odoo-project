4d4b5b46aa2eea1b719eb29849a75ae173f64761
Leeroy Brun <leeroy.brun@gmail.com>
2025-09-24 16:10:45 +0200

    feat(dockerize): support multiple archs install

--- install/dockerize.sh
+++ install/dockerize.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 set -eo pipefail
 
-curl -o dockerize-linux-amd64-v0.6.0.tar.gz https://github.com/jwilder/dockerize/releases/download/v0.6.0/dockerize-linux-amd64-v0.6.0.tar.gz -SL
-echo 'a13ff2aa6937f45ccde1f29b1574744930f5c9a5 dockerize-linux-amd64-v0.6.0.tar.gz' | sha1sum -c -
-tar xvfz dockerize-linux-amd64-v0.6.0.tar.gz -C /usr/local/bin && rm dockerize-linux-amd64-v0.6.0.tar.gz
+DOCKERIZE_VERSION=v0.9.6
+
+ARCH=$(uname -m)
+case "$ARCH" in
+    x86_64)
+        DOCKERIZE_ARCH=amd64
+        CHECKSUM="82b1159e8faa1b30b80d238bac91894ddd20b5e07543911cf5a3d400e17fa214"
+        ;;
+    aarch64)
+        DOCKERIZE_ARCH=arm64
+        CHECKSUM="857456036b139a27152230f28751977ee7e39f22b6a2b1c447319adb616c1aa0"
+        ;;
+    armv7l)
+        DOCKERIZE_ARCH=armhf
+        CHECKSUM="966f6b7042ba79bd5b22b74ed946d39362f4996a022f207e1fd10be276d1b3f6"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+URL="https://github.com/jwilder/dockerize/releases/download/${DOCKERIZE_VERSION}/dockerize-linux-${DOCKERIZE_ARCH}-${DOCKERIZE_VERSION}.tar.gz"
+
+echo "Downloading dockerize from $URL"
+curl -sSL -o dockerize.tar.gz "$URL"
+
+# Verify checksum
+echo "${CHECKSUM}  dockerize.tar.gz" | sha256sum -c -
+
+# Install
+tar -C /usr/local/bin -xzf dockerize.tar.gz
+rm dockerize.tar.gz
